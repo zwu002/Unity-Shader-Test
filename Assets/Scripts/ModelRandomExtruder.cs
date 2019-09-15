3d880fcc83978a57b176ead78a1fa705efc365c8
PotatoWu <z.wu233@gmail.com>
2019-09-15 17:20:02 +0100

    Extrusion shader and a random normal offset script

---
+++ Assets/Scripts/ModelRandomExtruder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRandomExtruder : MonoBehaviour
+{
+
+    void Start()
+    {
+        float amount = Random.Range(-0.0001f, 0.0001f);
+
+        Material material = GetComponent<Renderer>().sharedMaterial;
+        Material newMaterial = new Material(material);
+        newMaterial.SetFloat("_Amount", amount);
+        GetComponent<Renderer>().material = newMaterial;
+    }
+
+}
